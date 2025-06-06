b7977d794d521233646a998b510ad94568c14a01
muad1dib <163062034+muad1dib@users.noreply.github.com>
2025-06-05 23:44:20 -0300

    Add files via upload

---
+++ top_module_tb/tb_top_module.vhdl
@@ -0,0 +1,10 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity top_module_tb is
+end top_module_tb
+
+architecture Behavioral of top_module tb is
+    component fila
+
